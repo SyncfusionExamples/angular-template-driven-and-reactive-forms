316ec5cc80415aa04736336748d4d6aa2307f479
lakindu hewawasam <lakindu2002@live.com>
2022-03-19 00:21:51 +0530

    added the two form type components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import { TemplateDrivenComponent } from './components/template-driven/template-driven.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent,
+    TemplateDrivenComponent
   ],
   imports: [
     BrowserModule
