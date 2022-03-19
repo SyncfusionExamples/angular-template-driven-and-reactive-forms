937f8a95318e1040b2c473d197d29869eebcdb1c
lakindu hewawasam <lakindu2002@live.com>
2022-03-19 17:08:46 +0530

    added the reactive forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -14,7 +14,8 @@ import { TemplateDrivenComponent } from './components/template-driven/template-d
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
